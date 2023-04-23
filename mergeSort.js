9a79413bab407535035998627be7a70d96b36a54
Fabi0o <marfab32@gmail.com>
2023-04-23 15:52:26 +0200

    add mergesort agorithm that returne sorted array without duplicates

---
+++ mergeSort.js
@@ -0,0 +1,23 @@
+const mergeSort = (arr) => {
+  if (arr.length < 2) return arr;
+
+  const firstHalf = mergeSort(arr.slice(0, Math.floor(arr.length / 2)));
+  const secondHalf = mergeSort(arr.slice(Math.floor(arr.length / 2)));
+
+  return mergeSortedArrs(firstHalf, secondHalf);
+};
+
+const mergeSortedArrs = (arr1, arr2, result = []) => {
+  if (!arr1.length && !arr2.length) return result;
+  else if (!arr1.length) result.push(arr2.shift());
+  else if (!arr2.length) result.push(arr1.shift());
+  else if (arr1[0] == arr2[0]) {
+    result.push(arr1.shift());
+    arr2.shift();
+  } else
+    arr1[0] > arr2[0] ? result.push(arr2.shift()) : result.push(arr1.shift());
+
+  return mergeSortedArrs(arr1, arr2, result);
+};
+
+console.log(mergeSort([1, 1, 1, 1, 1, 1, 1, 1, 2]));
