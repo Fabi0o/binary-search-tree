89770b5285db8a03c15a495ace7cc8397ac78180
Fabi0o <marfab32@gmail.com>
2023-04-23 16:11:22 +0200

    code refactor for readability

--- Node.js
+++ Node.js
@@ -1,7 +1,7 @@
 export default class Node {
   constructor(value, right = null, left = null) {
     this.value = value;
-    this.left = right;
     this.right = left;
+    this.left = right;
   }
 }
