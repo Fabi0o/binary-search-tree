77457bd97c545a0ae49825f513b44bc7af186b00
Fabi0o <marfab32@gmail.com>
2023-04-23 16:58:55 +0200

    add export statement

--- Tree.js
+++ Tree.js
@@ -1,10 +1,11 @@
 import mergeSort from "./mergeSort.js";
 import Node from "./Node.js";
 
-class Tree {
+export default class Tree {
   constructor(arr) {
     this.root = buildTree(mergeSort(arr));
   }
+
   print(node = this.root, prefix = "", isLeft = true) {
     if (node === null) {
       return;
@@ -22,27 +23,12 @@ class Tree {
 const buildTree = (arr) => {
   if (arr.length == 1) return new Node(arr[0]);
   if (!arr[0]) return null;
+
   const mid = Math.floor(arr.length / 2);
+
   return new Node(
     arr[mid],
     buildTree(arr.slice(0, mid)),
     buildTree(arr.slice(mid + 1))
   );
 };
-
-const prettyPrint = (node, prefix = "", isLeft = true) => {
-  if (node === null) {
-    return;
-  }
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-  }
-  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-  }
-};
-
-const binaryTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-
-binaryTree.print();
