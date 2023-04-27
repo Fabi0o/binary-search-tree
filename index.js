c5250c74628087ea23045d7f4f18ab5b9dbebe63
Fabi0o <marfab32@gmail.com>
2023-04-27 12:47:25 +0200

    add driver script to index.js to test all functionalities

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
 import Tree from "./Tree.js";
 
-const binaryTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+const randomArray = (n) => {
+  const nums = [];
+
+  while (nums.length < n) {
+    nums.push(Math.floor(Math.random() * 100) + 1);
+  }
+  return nums;
+};
+
+console.log("Generating random balanced binary search tree...");
+
+const binaryTree = new Tree(randomArray(10));
 
 binaryTree.print();
+console.log(
+  binaryTree.isBalanced()
+    ? "The tree is balanced!"
+    : "The tree is not balanced!"
+);
+
+console.log(`Level order: ${binaryTree.levelOrder()}`);
+console.log(`Inorder: ${binaryTree.inOrder()}`);
+console.log(`Preorder: ${binaryTree.preOrder()}`);
+console.log(`Postorder: ${binaryTree.postOrder()}`);
+
+console.log("Unbalancing the tree by adding random numbers to it...");
+randomArray(100).forEach((num) => binaryTree.insert(num));
+binaryTree.print();
+
+console.log(
+  binaryTree.isBalanced()
+    ? "The tree is still balanced!"
+    : "The tree is now not balanced!"
+);
+
+console.log("Balancing the tree...");
+binaryTree.rebalance();
+binaryTree.print();
+console.log(
+  binaryTree.isBalanced()
+    ? "The tree is now balanced!"
+    : "The tree is still not balanced!"
+);
+
+console.log(`Level order: ${binaryTree.levelOrder()}`);
+console.log(`Inorder: ${binaryTree.inOrder()}`);
+console.log(`Preorder: ${binaryTree.preOrder()}`);
+console.log(`Postorder: ${binaryTree.postOrder()}`);
